d97caae123b7b8e4e0dbbbceeb07f7ace99eec9d
Vlad <vlad7772004@gmail.com>
2024-05-26 08:14:18 +0300

    Change base model

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import joblib
 
 
@@ -103,84 +104,176 @@ credit_df_columns = [
 ]
 
 
-def prepare_base_df(user_data, credit_df, card_df):
-	# User data
-	df = pd.DataFrame([user_data])
-	df = df.drop(columns=["JOB", "EMPLOYEE_FLAG"])
-	df = df.rename(columns={"GENDER": "MAN"})
+# def prepare_base_df(user_df, credit_df, card_df):
+# 	credit_df = credit_df.copy()
+# 	card_df = card_df.copy()
 
-	df["MAN"] = (df["MAN"] - 1).astype("bool")
+# 	# User data
+# 	# df = pd.DataFrame([user_data])
+# 	df = user_df.copy()
+# 	df = df.drop(columns=["JOB", "EMPLOYEE_FLAG"])
+# 	df = df.rename(columns={"GENDER": "MAN"})
 
-	# Credit data
-	credit_df["TERM"] = credit_df["TERM"].map(lambda x: x[:-1]).astype("int64")
+# 	df["MAN"] = (df["MAN"] - 1).astype("bool")
 
-	current_date = pd.Timestamp.now()
-	def mounth_until_end(open_date, term):
-		mounth_passed = (current_date.year - open_date.year) * 12 + current_date.month - open_date.month
-		reamin_mounths = term - mounth_passed
-		return max(0, reamin_mounths)
+# 	# Credit data
+# 	credit_df["TERM"] = credit_df["TERM"].map(lambda x: x[:-1]).astype("int64")
 
-	credit_df["REAMIN_MOUNTHS"] = credit_df.apply(lambda row: mounth_until_end(row['VALUE_DT'], row['TERM']), axis=1)
-	credit_df = credit_df.rename(columns={"OVERDUE_IND": "delay"})
+# 	current_date = pd.Timestamp.now()
+# 	def mounth_until_end(open_date, term):
+# 		mounth_passed = (current_date.year - open_date.year) * 12 + current_date.month - open_date.month
+# 		reamin_mounths = term - mounth_passed
+# 		return max(0, reamin_mounths)
 
-	# Cards data
-	card_df = card_df.drop(columns=["CARD_TYPE", "OPEN_DT"])
-	card_df = card_df.rename(columns={"СС_OVERDUE_IND": "delay"})
+# 	credit_df["REAMIN_MOUNTHS"] = credit_df.apply(lambda row: mounth_until_end(row['VALUE_DT'], row['TERM']), axis=1)
+# 	credit_df = credit_df.rename(columns={"OVERDUE_IND": "delay"})
 
-	def replace_product_code(value):
-		value = value.lower()
-		if 'grace' in value:
-			return 'grace'
-		elif 'blockbuster' in value:
-			return 'blockbuster'
-		elif '_bb_' in value:
-			return 'bb'
+# 	# Cards data
+# 	card_df = card_df.drop(columns=["CARD_TYPE", "OPEN_DT"])
+# 	card_df = card_df.rename(columns={"СС_OVERDUE_IND": "delay"})
 
-		return 'other'
+# 	def replace_product_code(value):
+# 		value = value.lower()
+# 		if 'grace' in value:
+# 			return 'grace'
+# 		elif 'blockbuster' in value:
+# 			return 'blockbuster'
+# 		elif '_bb_' in value:
+# 			return 'bb'
 
-	card_df["PRODUCT_CODE"] = card_df["PRODUCT_CODE"].apply(replace_product_code)
+# 		return 'other'
 
-	# Merging
-	## Preparing dataframes
-	df_paid_off_counts = credit_df[credit_df["REAMIN_MOUNTHS"] == 0].groupby("CLIENT_ID").size().reset_index(name="paid_off_count")
-	df_active_counts = credit_df[credit_df["REAMIN_MOUNTHS"] > 0].groupby("CLIENT_ID").size().reset_index(name="active_count")
-	df_active_sums = credit_df[credit_df["REAMIN_MOUNTHS"] > 0].groupby("CLIENT_ID")["ORIG_AMOUNT"].sum().reset_index(name="active_sum")
-	df_max_term = credit_df.groupby('CLIENT_ID')['TERM'].max().reset_index(name='max_term')
-	df_delay_counts = credit_df[credit_df['delay'] > 0].groupby("CLIENT_ID").size().reset_index(name="credits_delay_count")
+# 	card_df["PRODUCT_CODE"] = card_df["PRODUCT_CODE"].apply(replace_product_code)
 
-	df_cards_count = card_df.groupby('CLIENT_ID').size().reset_index(name="cards_count")
-	df_cards_delay_count = card_df[card_df['delay'] > 0].groupby('CLIENT_ID').size().reset_index(name="cards_delay_count")
+# 	# Merging
+# 	## Preparing dataframes
+# 	df_paid_off_counts = credit_df[credit_df["REAMIN_MOUNTHS"] == 0].groupby("CLIENT_ID").size().reset_index(name="paid_off_count")
+# 	df_active_counts = credit_df[credit_df["REAMIN_MOUNTHS"] > 0].groupby("CLIENT_ID").size().reset_index(name="active_count")
+# 	df_active_sums = credit_df[credit_df["REAMIN_MOUNTHS"] > 0].groupby("CLIENT_ID")["ORIG_AMOUNT"].sum().reset_index(name="active_sum")
+# 	df_max_term = credit_df.groupby('CLIENT_ID')['TERM'].max().reset_index(name='max_term')
+# 	df_delay_counts = credit_df[credit_df['delay'] > 0].groupby("CLIENT_ID").size().reset_index(name="credits_delay_count")
 
-	## Merge dataframes
-	df = pd.merge(df, df_paid_off_counts, on='CLIENT_ID', how='left')
-	df = pd.merge(df, df_active_counts, on='CLIENT_ID', how='left')
-	df = pd.merge(df, df_active_sums, on='CLIENT_ID', how='left')
-	df = pd.merge(df, df_max_term, on='CLIENT_ID', how='left')
-	df = pd.merge(df, df_delay_counts, on='CLIENT_ID', how='left')
+# 	df_cards_count = card_df.groupby('CLIENT_ID').size().reset_index(name="cards_count")
+# 	df_cards_delay_count = card_df[card_df['delay'] > 0].groupby('CLIENT_ID').size().reset_index(name="cards_delay_count")
 
-	df = pd.merge(df, df_cards_count, on='CLIENT_ID', how='left')
-	df = pd.merge(df, df_cards_delay_count, on='CLIENT_ID', how='left')
+# 	## Merge dataframes
+# 	df = pd.merge(df, df_paid_off_counts, on='CLIENT_ID', how='left')
+# 	df = pd.merge(df, df_active_counts, on='CLIENT_ID', how='left')
+# 	df = pd.merge(df, df_active_sums, on='CLIENT_ID', how='left')
+# 	df = pd.merge(df, df_max_term, on='CLIENT_ID', how='left')
+# 	df = pd.merge(df, df_delay_counts, on='CLIENT_ID', how='left')
 
-	## Filling empty data
-	df['paid_off_count'] = df['paid_off_count'].fillna(0).astype('int64')
-	df['active_count'] = df['active_count'].fillna(0).astype('int64')
-	df['max_term'] = df['max_term'].fillna(0).astype('int64')
-	df['active_sum'] = df['active_sum'].fillna(0)
-	df['credits_delay_count'] = df['credits_delay_count'].fillna(0).astype('int64')
+# 	df = pd.merge(df, df_cards_count, on='CLIENT_ID', how='left')
+# 	df = pd.merge(df, df_cards_delay_count, on='CLIENT_ID', how='left')
 
-	df['cards_count'] = df['cards_count'].fillna(0).astype('int64')
-	df['cards_delay_count'] = df['cards_delay_count'].fillna(0).astype('int64')
+# 	## Filling empty data
+# 	df['paid_off_count'] = df['paid_off_count'].fillna(0).astype('int64')
+# 	df['active_count'] = df['active_count'].fillna(0).astype('int64')
+# 	df['max_term'] = df['max_term'].fillna(0).astype('int64')
+# 	df['active_sum'] = df['active_sum'].fillna(0)
+# 	df['credits_delay_count'] = df['credits_delay_count'].fillna(0).astype('int64')
 
-	# Process object columns
-	df = pd.get_dummies(df, columns=['MARITAL_STATUS'], prefix='MATRIAL')
-	df = df.drop(columns=['REGION', 'ORGANIZATION'], axis=1)
-	df = df.replace({False: 0, True: 1})
+# 	df['cards_count'] = df['cards_count'].fillna(0).astype('int64')
+# 	df['cards_delay_count'] = df['cards_delay_count'].fillna(0).astype('int64')
 
-	for column in base_df_columns:
-		if column not in df.columns:
-			df[column] = 0
+# 	# Process object columns
+# 	df = pd.get_dummies(df, columns=['MARITAL_STATUS'], prefix='MATRIAL')
+# 	df = df.drop(columns=['REGION', 'ORGANIZATION'], axis=1)
+# 	df = df.replace({False: 0, True: 1})
 
-	return df[base_df_columns]
+# 	for column in base_df_columns:
+# 		if column not in df.columns:
+# 			df[column] = 0
+
+# 	return df[base_df_columns]
+
+
+import pandas as pd
+
+def prepare_base_df(user_df, credit_df, card_df):
+    credit_df = credit_df.copy()
+    card_df = card_df.copy()
+
+    # User data
+    df = user_df.copy()
+    df = df.drop(columns=["JOB", "EMPLOYEE_FLAG"])
+    df = df.rename(columns={"GENDER": "MAN"})
+
+    df["MAN"] = (df["MAN"] - 1).astype("bool")
+
+    # Credit data
+    credit_df["TERM"] = credit_df["TERM"].map(lambda x: x[:-1]).astype("int64")
+
+    current_date = pd.Timestamp.now()
+    
+    def mounth_until_end(open_date, term):
+        mounth_passed = (current_date.year - open_date.year) * 12 + current_date.month - open_date.month
+        remain_months = term - mounth_passed
+        return max(0, remain_months)
+
+    credit_df["REMAIN_MONTHS"] = credit_df.apply(lambda row: mounth_until_end(row['VALUE_DT'], row['TERM']), axis=1)
+    credit_df = credit_df.rename(columns={"OVERDUE_IND": "delay"})
+
+    # Cards data
+    card_df = card_df.drop(columns=["CARD_TYPE", "OPEN_DT"])
+    card_df = card_df.rename(columns={"СС_OVERDUE_IND": "delay"})
+
+    def replace_product_code(value):
+        value = value.lower()
+        if 'grace' in value:
+            return 'grace'
+        elif 'blockbuster' in value:
+            return 'blockbuster'
+        elif '_bb_' in value:
+            return 'bb'
+        return 'other'
+
+    card_df["PRODUCT_CODE"] = card_df["PRODUCT_CODE"].apply(replace_product_code)
+
+    # Merging
+    ## Preparing dataframes
+    df_paid_off_counts = credit_df[credit_df["REMAIN_MONTHS"] == 0].groupby("CLIENT_ID").size().reset_index(name="paid_off_count")
+    df_active_counts = credit_df[credit_df["REMAIN_MONTHS"] > 0].groupby("CLIENT_ID").size().reset_index(name="active_count")
+    df_active_sums = credit_df[credit_df["REMAIN_MONTHS"] > 0].groupby("CLIENT_ID")["ORIG_AMOUNT"].sum().reset_index(name="active_sum")
+    df_max_term = credit_df.groupby('CLIENT_ID')['TERM'].max().reset_index(name='max_term')
+    df_delay_counts = credit_df[credit_df['delay'] > 0].groupby("CLIENT_ID").size().reset_index(name="credits_delay_count")
+
+    df_cards_count = card_df.groupby('CLIENT_ID').size().reset_index(name="cards_count")
+    df_cards_delay_count = card_df[card_df['delay'] > 0].groupby('CLIENT_ID').size().reset_index(name="cards_delay_count")
+
+    ## Merge dataframes
+    df = pd.merge(df, df_paid_off_counts, on='CLIENT_ID', how='left')
+    df = pd.merge(df, df_active_counts, on='CLIENT_ID', how='left')
+    df = pd.merge(df, df_active_sums, on='CLIENT_ID', how='left')
+    df = pd.merge(df, df_max_term, on='CLIENT_ID', how='left')
+    df = pd.merge(df, df_delay_counts, on='CLIENT_ID', how='left')
+
+    df = pd.merge(df, df_cards_count, on='CLIENT_ID', how='left')
+    df = pd.merge(df, df_cards_delay_count, on='CLIENT_ID', how='left')
+
+    ## Filling empty data
+    df['paid_off_count'] = df['paid_off_count'].fillna(0).astype('int64')
+    df['active_count'] = df['active_count'].fillna(0).astype('int64')
+    df['max_term'] = df['max_term'].fillna(0).astype('int64')
+    df['active_sum'] = df['active_sum'].fillna(0)
+    df['credits_delay_count'] = df['credits_delay_count'].fillna(0).astype('int64')
+
+    df['cards_count'] = df['cards_count'].fillna(0).astype('int64')
+    df['cards_delay_count'] = df['cards_delay_count'].fillna(0).astype('int64')
+
+    # Process object columns
+    df = pd.get_dummies(df, columns=['MARITAL_STATUS'], prefix='MARITAL')
+    df = df.drop(columns=['REGION', 'ORGANIZATION'], axis=1)
+    df = df.replace({False: 0, True: 1})
+
+    # Ensure all columns in base_df_columns are present in the DataFrame
+    for column in base_df_columns:
+        if column not in df.columns:
+            df[column] = 0
+
+    return df[base_df_columns]
+
+
 
 
 def prepare_card_df(base_df, card_df):
@@ -233,6 +326,24 @@ def prepare_credit_df(base_df, credit_df, credit_data):
 	return df[credit_df_columns]
 
 
+def create_user_data(row):
+	return {
+		"CLIENT_ID": row['CLIENT_ID'],
+		"AGE": row['AGE'],
+		"REGION": row['REGION'],
+		"GENDER": row['GENDER'],
+		"JOB": row['JOB'],
+		"ORGANIZATION": row['ORGANIZATION'],
+		"INCOME": row['INCOME'],
+		"MARITAL_STATUS": row['MARITAL_STATUS'],
+		"IP_FLAG": row['IP_FLAG'],
+		"SME_FLAG": row['SME_FLAG'],
+		"EMPLOYEE_FLAG": row['EMPLOYEE_FLAG'],
+		"REFUGEE_FLAG": row['REFUGEE_FLAG'],
+		"PDN": row['PDN'],
+	}
+
+
 def main():
 	credit_df = pd.read_excel("data/raw/credit_ds.xlsx")
 	card_df = pd.read_excel("data/raw/card_ds.xlsx")
@@ -252,35 +363,59 @@ def main():
 		"PDN": 26.6,
 	}
 
-	# credit_data = {
-	# 	# "CLIENT_ID": 9039,
-	# 	"CREDIT_TYPE": 501,
-	# 	"CREDIT_PURCHASE": "Автокредит",
-	# 	"PRODUCT_CODE": "Standard Installment Loan",
-	# 	"TERM": 36,
-	# 	"ORIG_AMOUNT": 69831,
-	# 	"CURR_RATE_NVAL": 8.49,
-	# 	"VALUE_DT": pd.to_datetime("2020-02-05"),
-	# }
-
 	model = joblib.load("params/dt_model.joblib")
 
 	base_df = prepare_base_df(user_data, credit_df, card_df)
-	# card_df = prepare_card_df(base_df, card_df)
-	# credit_df = prepare_credit_df(base_df, credit_df, credit_data)
-
-	print(base_df.info())
-	# print(card_df.info())
-	# print(credit_df.info())
-
-	# card_df = card_df.drop(columns=['CLIENT_ID'])
-	# credit_df = credit_df.drop(columns=['CLIENT_ID'])
-
 	base_df = base_df.drop(columns=["CLIENT_ID"])
+
 	pred = model.predict_proba(base_df)[0][0] * 100
 	print("=" * 16)
 	print(f"Client will repay the loan with probability: {pred:.2f}%")
 
 
+def gen_file():
+	credit_df = pd.read_excel("data/raw/credit_ds.xlsx")
+	card_df = pd.read_excel("data/raw/card_ds.xlsx")
+
+	model = joblib.load("params/rf_model.joblib")
+	user_df = pd.read_excel("data/val.xlsx")
+
+	base_df = prepare_base_df(user_df, credit_df, card_df).fillna(0)
+	print(base_df.info())
+	print(base_df[["credits_delay_count", "active_count", "active_sum", "max_term"]].describe())
+
+	# print(set(base_df['CLIENT_ID'].values).intersection(set(credit_df['CLIENT_ID'].values)))
+	# print(set(base_df['CLIENT_ID'].values).intersection(set(card_df['CLIENT_ID'].values)))
+
+	base_df = base_df.drop(columns=["credits_delay_count",
+									"cards_delay_count",
+									"paid_off_count",
+									"active_sum",
+									"max_term",
+									"CLIENT_ID"])
+
+	p = model.predict_proba(base_df)
+	res = []
+	for t, f in p:
+		if f > 0.4:
+			res.append(1)
+		else:
+			res.append(0)
+
+	# print(res)
+	user_df['unreliable'] = res
+	output_file = "data/val_out.csv"
+	user_df.to_csv(output_file, index=False)
+	# print(np.max(p, axis=0))
+	# print(np.sum(p, axis=0))
+	# for _, row in df.iterrows():
+	# 	user_data = create_user_data(row)
+	# 	base_df = prepare_base_df(user_data, credit_df, card_df)
+		# pred = model.predict_proba(base_df.drop(columns=["CLIENT_ID"]))[0][0] * 100
+
+	# 	print(pred)
+
+
 if __name__ == '__main__':
-	main()
+	# main()
+	gen_file()
